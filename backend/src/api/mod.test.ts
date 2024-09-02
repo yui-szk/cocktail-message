c6dd5c61fb725b87f55984ea70872d5b69bdbff7
Souma <101255979+5ouma@users.noreply.github.com>
2024-09-02 10:58:47 +0900

    feat(backend): Route API endpoints
    Under the `/api`, data can be got by several ways.

---
+++ backend/src/api/mod.test.ts
@@ -0,0 +1,47 @@
+import { assertEquals, assertExists } from "@std/assert";
+import { STATUS_CODE } from "@std/http/status";
+
+import { app } from "./mod.ts";
+
+Deno.test("API", async (t: Deno.TestContext) => {
+  await t.step("GET /", async () => {
+    const res: Response = await app.request("/");
+
+    assertEquals(await res.text(), "Cocktail API");
+    assertEquals(res.status, STATUS_CODE.OK);
+  });
+
+  await t.step("GET /get", async () => {
+    const res: Response = await app.request("/get");
+
+    assertEquals(await res.json(), {
+      success: false,
+      message: "The name query is required",
+    });
+    assertEquals(res.status, STATUS_CODE.BadRequest);
+  });
+
+  await t.step("GET /get?name=none", async () => {
+    const res: Response = await app.request("/get?name=none");
+
+    assertEquals(await res.json(), {
+      success: false,
+      message: 'The cocktail, "none" not found',
+    });
+    assertEquals(res.status, STATUS_CODE.NotFound);
+  });
+
+  await t.step("GET /get?name=アイリッシュコーヒー", async () => {
+    const res: Response = await app.request("/get?name=アイリッシュコーヒー");
+
+    assertExists(await res.json());
+    assertEquals(res.status, STATUS_CODE.OK);
+  });
+
+  await t.step("/all", async () => {
+    const res: Response = await app.request("/all");
+
+    assertExists(await res.json());
+    assertEquals(res.status, STATUS_CODE.OK);
+  });
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import { type Context, Hono } from "@hono/hono";
 export type { Hono };
 import { logger } from "@hono/hono/logger";
 
+import { app as api } from "./api/mod.ts";
+
 /**
  * The Hono application for this project.
  *
@@ -13,6 +15,7 @@ import { logger } from "@hono/hono/logger";
 export const app: Hono = new Hono();
 app.use(logger());
 app
-  .get("/", (ctx: Context) => ctx.text("Hello, World!"));
+  .get("/", (ctx: Context) => ctx.text("Hello, World!"))
+  .route("/api", api);
 
 Deno.serve(app.fetch);

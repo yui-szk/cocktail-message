91844b12ad31a6117ac5e3b90a3ba191ebfce872
yui-szk <yuisuzuki.017@gmail.com>
2024-09-02 14:14:00 +0900

    Update color variable

--- src/layout/WithHTMX.tsx
+++ src/layout/WithHTMX.tsx
@@ -7,17 +7,24 @@ const layoutStyle = css`
       margin: 0;
       padding: 0;
       box-sizing: border-box;
+
+      --color-black: #000000;
+      --color-white: #ffffff;
+      --color-main: #443d3a;
+      --color-base: #e9e3dd;
+      --color-accent: #952b12;
+      --color-stroke: #afafaf;
     }
     :root {
       font-family: Noto-Sans;
-      color: rgba(255, 255, 255);
-      background-color: #242424;
+      color: #ffffff;
+      background-color: #443d3a;
     }
     html {
       font-size: 62.5%;
     }
     body {
-      margin: 0 1.25rem;
+      margin: 0 3rem;
     }
     h1 {
       font-size: 3rem;
@@ -29,7 +36,7 @@ export function WithHTMX({ children }: { children: unknown }) {
   return (
     <HTML>
       <head>
-        <title>Deno + Hono + htmx</title>
+        <title>cocktail message</title>
         <Style />
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />

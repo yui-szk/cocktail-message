ab9a88b07652007f82766678f3ee5b1d8f4dc947
Souma <101255979+5ouma@users.noreply.github.com>
2024-09-02 09:28:12 +0900

    chore(init): Deno project (#11)
    Add a sample script with Hono and some useful features.

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import { type Context, Hono } from "@hono/hono";
+export type { Hono };
+import { logger } from "@hono/hono/logger";
+
+/**
+ * The Hono application for this project.
+ *
+ * @example
+ * ```ts
+ * const res: Response = await app.request("/");
+ * ```
+ */
+export const app: Hono = new Hono();
+app.use(logger());
+app
+  .get("/", (ctx: Context) => {
+    return ctx.text("Hello, World!");
+  });
+
+Deno.serve(app.fetch);

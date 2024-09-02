bd99cc00293c9f4dab985a8ec818eb5c39263de9
yui-szk <yuisuzuki.017@gmail.com>
2024-09-03 04:16:42 +0900

    Add router of createMessage

---
+++ frontend/src/components/router.tsx
@@ -0,0 +1,22 @@
+import { Hono } from "@hono/hono";
+import { serveStatic } from "@hono/hono/deno";
+import { CreateMessageTop } from "./CreateMessageTop.tsx";
+import { CreateMessageSelect } from "./CreateMessageSelect.tsx";
+import { CreateMessageCheck } from "./CreateMessageCheck.tsx";
+
+export const createMessage = new Hono();
+
+createMessage
+  .get("/", (ctx) => {
+    return ctx.html(<CreateMessageTop />);
+  })
+  .get("/select", (ctx) => {
+    return ctx.html(<CreateMessageSelect />);
+  })
+  .get("/check", (ctx) => {
+    return ctx.html(<CreateMessageCheck />);
+  })
+  .use(
+    "/search-icon.svg",
+    serveStatic({ path: "./frontend/src/images/search-icon.svg" })
+  );

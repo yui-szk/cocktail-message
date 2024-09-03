e73b8a13fa8079eeab7b130061624b992346eebb
Souma <101255979+5ouma@users.noreply.github.com>
2024-09-03 11:09:59 +0900

    feat(backend): Add message API endpoints (#17)
    An API to get the data in the `/backend/private/messages.jsonc`.

--- backend/src/api/utils/data.ts
+++ backend/src/api/utils/data.ts
@@ -1,6 +1,6 @@
 import { parse } from "@std/jsonc";
 
-import type { Cocktail } from "../utils/types.ts";
+import type { Cocktail, Message } from "../utils/types.ts";
 
 const cocktailFile = "./private/cocktails.jsonc";
 const cocktailData: string = await Deno.readTextFile(cocktailFile);
@@ -14,3 +14,16 @@ const cocktailData: string = await Deno.readTextFile(cocktailFile);
  * ```
  */
 export const cocktails: Cocktail[] = parse(cocktailData) as Cocktail[];
+
+const messageFile = "./private/messages.jsonc";
+const messageData: string = await Deno.readTextFile(messageFile);
+
+/**
+ * An array of messages data
+ *
+ * @example
+ * ```ts
+ * console.log(messages);
+ * ```
+ */
+export const messages: Message[] = parse(messageData) as unknown as Message[];

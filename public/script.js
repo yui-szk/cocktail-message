b3f4735024e609dea0d313de09d6264eadf20940
Ryo Hodota <45889934+Ryo-7@users.noreply.github.com>
2024-09-04 13:49:06 +0900

    chore(fix): Auto fixes from pre-commit.com hooks
    For more information, see https://pre-commit.ci.

--- public/script.js
+++ public/script.js
@@ -16,24 +16,24 @@ window.addEventListener('load', (event) => {
             console.log('Received data:', data); // 取得したデータをコンソールに出力
             // ここで、データを使った他の処理を実行することができます
             const ul = document.getElementById('kensaku_result');
-            
+
             for(i = 0; i < data.length; i++){
                 // li 要素を作成
                 const li = document.createElement('li');
-                
+
                 // li 要素に onclick イベントを追加
                 li.setAttribute('onclick', 'buttonclick(this)');
-                
+
                 // li のテキストコンテンツを追加
                 li.textContent = data[i].word;
-                
+
                 // small 要素を作成してテキストを追加
                 const small = document.createElement('small');
                 small.textContent = data[i].name;
-                
+
                 // li に small 要素を追加
                 li.appendChild(small);
-                
+
                 // ul に li 要素を追加
                 ul.appendChild(li);
             }

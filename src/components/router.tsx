fb90358d6fe3785822566ff73eaa9bcd90b65a39
yui-szk <yuisuzuki.017@gmail.com>
2024-09-05 10:37:30 +0900

    Add routing

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,4 +1,6 @@
 import { Hono } from "@hono/hono";
+
+import { Home } from "../pages/Home.tsx";
 import { CreateMessageTop } from "./CreateMessageTop.tsx";
 import { CreateMessageSelect } from "./CreateMessageSelect.tsx";
 import { CreateMessageCheck } from "./CreateMessageCheck.tsx";
@@ -10,6 +12,7 @@ import { CreateMessageCheck } from "./CreateMessageCheck.tsx";
 export const createMessage = new Hono();
 
 createMessage
-  .get("/", (ctx) => ctx.html(<CreateMessageTop />))
+  .get("/", (ctx) => ctx.html(<Home />))
+  .get("/create", (ctx) => ctx.html(<CreateMessageTop />))
   .get("/select", (ctx) => ctx.html(<CreateMessageSelect />))
   .get("/check", (ctx) => ctx.html(<CreateMessageCheck />));

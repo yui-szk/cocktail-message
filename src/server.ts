272c0da10051170d3d8d3880541e0dc049b865e0
Ryo Hodota <45889934+Ryo-7@users.noreply.github.com>
2024-09-06 15:20:16 +0900

    一旦

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { Hono } from "@hono/hono";
 export type { Hono };
 import { logger } from "@hono/hono/logger";
 import { serveStatic } from "@hono/hono/deno";
-
+import {cors} from "@hono/hono/cors"
 import { createMessage } from "./components/router.tsx";
 import { app as api } from "./api/mod.ts";
 
@@ -17,6 +17,14 @@ import { app as api } from "./api/mod.ts";
 export const app: Hono = new Hono();
 app.use(logger());
 app
+  .use("/api",  cors({
+    origin: ['https://rod.expfrom.me', 'http://localhost:8000'],
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // ここに追加
+    allowMethods: ['POST', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  }))
   .route("/", createMessage)
   .route("/api", api)
   .use("/favicon.ico", serveStatic({ path: "./public/images/favicon.ico" }))
